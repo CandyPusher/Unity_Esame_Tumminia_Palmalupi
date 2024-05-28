0993652d436f448c29b8115659c51987ab0b70dc
MarcoTumminia <marco.tumminia@unirufa.it>
2024-05-28 11:57:51 +0200

    v0.9
    TMP button

--- Esame_Palmalupi_Tumminia/Assets/Script/Ui/ButtonUI.cs
+++ Esame_Palmalupi_Tumminia/Assets/Script/Ui/ButtonUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonUi : MonoBehaviour
+public class ButtonUI : MonoBehaviour
 {
     [SerializeField] private string NewGameLevel = "LV_1";
     public void NewGameButton()
